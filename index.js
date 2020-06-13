cbdc35ffd9eca5abe2b5edd2a433ac7fb59af697
kusigit <m.fuerer@vtxmail.ch>
2020-06-13 16:18:32 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// https://github.com/benlaplanche/cf-basic-auth-route-service/tree/master/servicebroker
+
+const cookie = require('cookie');
+const redis = require('./redis');
+
+module.exports = async (req, res, next) => {
+  const cookies = cookie.parse(req.headers.cookie || 'sid=');
+
+  const mandant = await redis.getAsync(cookies.sid);
+  if (mandant) {
+    // eslint-disable-next-line require-atomic-updates
+    req.mandant = mandant;
+    next();
+  } else {
+    res.status(401).send('unauthorized');
+  }
+};
