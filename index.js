45ca177853b60658f887dd82755ad7f4202788bb
Markus Fürer <markus.fuerer@imedico.ch>
2023-09-01 23:40:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 'use strict';
 
-// https://github.com/benlaplanche/cf-basic-auth-route-service/tree/master/servicebroker
-
 const cookie = require('cookie');
-const redis = require('redis-client');
+const Redis = require('ioredis');
+
+// https://github.com/cloud-gov/aws-redis-example/blob/main/node/README.md
+const options = {
+  host: process.env.REDIS_SERVICE_HOST || 'localhost',
+  tls: process.env.REDIS_SERVICE_HOST ? {} : null,
+};
+
+const redis = new Redis(options);
 
 module.exports = async (req, res, next) => {
   const cookies = cookie.parse(req.headers.cookie || 'sid=');
+  const mandant = await redis.get(cookies.sid);
 
-  const mandant = await redis.getAsync(cookies.sid);
   if (mandant) {
     // eslint-disable-next-line require-atomic-updates
     req.mandant = mandant;
